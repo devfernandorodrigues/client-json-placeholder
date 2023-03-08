29da481dc1b71f9466cf24e3ad7eab6c887ba8a4
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2023-03-07 22:26:13 -0300

    refactor: files

---
+++ jsonplaceholder/resources.py
@@ -0,0 +1,23 @@
+from jsonplaceholder.base import BaseResource
+from jsonplaceholder.request import request
+from jsonplaceholder.schemas import Comment, Post
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+
+
+class PostResource(BaseResource):
+    schema = Post
+    ENDPOINT = f"{BASE_URL}/posts"
+
+    def comments(self, _id: int):
+        resp = request("GET", f"{self.ENDPOINT}/{_id}/comments")
+        return [Comment(**data) for data in resp.json()]
+
+
+class CommentsResource(BaseResource):
+    ENDPOINT = f"{BASE_URL}/comments"
+    schema = Comment
+
+    def post_id(self, _id: int):
+        resp = request("GET", f"{self.ENDPOINT}?postId={_id}")
+        return [Comment(**data) for data in resp.json()]
